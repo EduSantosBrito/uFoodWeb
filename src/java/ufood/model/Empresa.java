77c8b23cc2ad72aca1d9a1ddfa06067b9eb27974
unknown <nicolas.cardoso@l05m07.academico-img.br>
2018-10-23 20:10:29 -0200

    Model da Empresa e Produto

---
+++ src/java/ufood/model/Empresa.java
@@ -0,0 +1,33 @@
+package ufood.model;
+
+public class Empresa {
+    
+    private Long idEmpresa;
+    private String nome;
+
+    public Empresa(Long idEmpresa, String nome) {
+        this.idEmpresa = idEmpresa;
+        this.nome = nome;
+    }
+
+    public Long getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public Empresa setIdEmpresa(Long idEmpresa) {
+        this.idEmpresa = idEmpresa;
+        return this;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+   
+    
+}
+
