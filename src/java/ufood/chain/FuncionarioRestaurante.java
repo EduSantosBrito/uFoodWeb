a940793d121b350a90d9ba639ea4891e3137f6ac
unknown <ismael.pereira@l05m06.academico-img.br>
2018-10-24 20:23:34 -0200

    Chain of responsability e identação

---
+++ src/java/ufood/chain/FuncionarioRestaurante.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufood.chain;
+
+/**
+ *
+ * @author ismael.pereira
+ */
+public class FuncionarioRestaurante extends ChainMae {
+
+    public FuncionarioRestaurante(ChainMae proximoPasso) {
+        super(proximoPasso);
+    }
+
+    @Override
+    public boolean isResolved(String problema) {
+        if (problema.equals("Entrega")) {
+            return true;
+        } else if (proximoPasso != null) {
+            return proximoPasso.isResolved(problema);
+        }
+        return false;
+    }
+}
--- src/java/ufood/dao/UsuarioDAO.java
+++ src/java/ufood/dao/UsuarioDAO.java
@@ -20,62 +20,72 @@ import ufood.persistence.DatabaseLocator;
  * @author Eduardo Santos
  */
 public class UsuarioDAO {
+
     private static UsuarioDAO userDAO = new UsuarioDAO();
     private Connection conn;
+
     public static UsuarioDAO getInstance() {
         return userDAO;
     }
-    private UsuarioDAO(){
+
+    private UsuarioDAO() {
     }
-     public void save(Usuario usuario)throws ClassNotFoundException, SQLException{
+
+    public void save(Usuario usuario) throws ClassNotFoundException, SQLException {
         Statement st = null;
-        
+
         try {
             conn = DatabaseLocator.getInstance().getConnection();
             st = conn.createStatement();
-            st.execute("insert into usuario(idUsuario, nome, senha) values ('" + usuario.getIdUsuario() 
+            st.execute("insert into usuario(idUsuario, nome, senha) values ('" + usuario.getIdUsuario()
                     + "', '" + usuario.getNome() + "', '" + usuario.getSenha());
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             throw e;
         } finally {
             closeResources(st, conn);
         }
     }
-      public void delete(Integer id) throws SQLException, ClassNotFoundException {
+
+    public void delete(Integer id) throws SQLException, ClassNotFoundException {
         Statement st = null;
         try {
             conn = DatabaseLocator.getInstance().getConnection();
             st = conn.createStatement();
             st.execute("delete from usuario where idUsuario = '" + id + "';");
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             throw e;
         } finally {
             closeResources(st, conn);
         }
-     }
-      public List<Usuario> read(Integer id) throws SQLException, ClassNotFoundException {
+    }
+
+    public List<Usuario> read(Integer id) throws SQLException, ClassNotFoundException {
         Statement st = null;
         List<Usuario> usuarios = new ArrayList<>();
         try {
             conn = DatabaseLocator.getInstance().getConnection();
             st = conn.createStatement();
             ResultSet rs = st.executeQuery("");
-            while(rs.next()){
-               usuarios.add(new Usuario(rs.getLong("idUsuario"),rs.getString("nome"),rs.getString("senha")));
+            while (rs.next()) {
+                usuarios.add(new Usuario(rs.getLong("idUsuario"), rs.getString("nome"), rs.getString("senha")));
             }
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             throw e;
         } finally {
             closeResources(st, conn);
         }
         return usuarios;
     }
-      
-          private void closeResources(Statement st, Connection conn) {
-       try {
-            if(st!=null) st.close();
-            if(conn!=null) conn.close();
-        } catch(SQLException e) {
+
+    private void closeResources(Statement st, Connection conn) {
+        try {
+            if (st != null) {
+                st.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
 
         }
     }
